a89d08a6fd77294bb5541e41a3803461d192cd5b
Red <vhanchar@ucsc.edu>
2019-08-16 08:57:51 -0700

    updated SmurfForm with formik

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import {withFormik, Form, Field} from 'formik';
+// import * as Yup from "yup";
+// import axios from 'axios';
 
-const SmurfForm = () => {
+
+function SmurfForm () {
     return(
-        <>
-        smurf 
-        </>
+        <Form className='smurfForm'>
+        {/* <h3> Add Smurf To Your Village: </h3> */}
+
+        <Field type='text' name='name' placeholder='smurf name' />
+        <Field type='number' name='age' placeholder='smurf age' />
+        <Field type='height' name='height' placeholder='smurf height' />
+
+        <button type='submit' className='add-smurf-button' > add
+        </button>
+
+        </Form>
     )
 }
 
-export default SmurfForm;
+const FormikSmurfForm = withFormik({
+    mapPropsToValues(name, age, height){
+        return{
+            // name: name || '',
+            age: age || '',
+            height: height || ''
+        }
+    }
+})(SmurfForm);
+
+export default FormikSmurfForm;
